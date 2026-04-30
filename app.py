52449f67f404da5be7c704f8c0879b3a9e5cdf83
DiyaMaheshwari2004 <maheshwaridiya2004@gmail.com>
2026-04-30 14:57:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel                                                                        
+import database  # creates DB on startup
 import sqlite3
 from chatbot import chatbot
 
